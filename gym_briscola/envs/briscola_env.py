0678f62407a5538aa25a8d708cf942ee185d1083
Ronaldo Vieira <ronaldoesv@gmail.com>
2019-07-09 16:57:57 -0300

    Create card ranks and suits

--- gym_briscola/envs/briscola_env.py
+++ gym_briscola/envs/briscola_env.py
@@ -1,5 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import gym
 import numpy as np
+
+from enum import Enum
+
+
+class Suit(Enum):
+    HEARTS = '♥'
+    DIAMONDS = '♦'
+    CLUBS = '♣'
+    SPADES = '♠'
+
+
+class Rank(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    JACK = 8
+    QUEEN = 9
+    KING = 10
+    ACE = 11
 
 
 class BiscolaEnv(gym.Env):
